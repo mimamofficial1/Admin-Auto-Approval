1829d6be26e8d8f63c30a4ecc82a10250dd0df4d
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-05-31 11:40:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ ADMINS = int(environ.get("ADMINS", "6139759254"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://mohammadmuzaffarimambaturbari:sHXNxpKZ9PDjyYQr@cluster0.dqjjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_NAME = environ.get("DB_NAME", "mohammadmuzaffarimambaturbari")
 
-# If this is True Then Bot Accept New Join Request 
+# If this is True Then Bot Accept New Join Request
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
