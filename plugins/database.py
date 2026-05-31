057ace7236e95dafe26562ee3626b196dff04a68
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-05-31 11:39:57 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -8,6 +8,7 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.admin_col = self.db.admins
 
     def new_user(self, id, name):
         return dict(
@@ -33,6 +34,29 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+    # ===== ADMIN SYSTEM =====
+
+    async def add_admin(self, user_id):
+        exists = await self.admin_col.find_one({'id': int(user_id)})
+        if not exists:
+            await self.admin_col.insert_one({'id': int(user_id)})
+            return True
+        return False
+
+    async def remove_admin(self, user_id):
+        result = await self.admin_col.delete_one({'id': int(user_id)})
+        return result.deleted_count > 0
+
+    async def get_all_admins(self):
+        admins = []
+        async for admin in self.admin_col.find({}):
+            admins.append(admin['id'])
+        return admins
+
+    async def is_admin(self, user_id):
+        admin = await self.admin_col.find_one({'id': int(user_id)})
+        return bool(admin)
 
 
 db = Database(DB_URI, DB_NAME)
